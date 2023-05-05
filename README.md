8a12427d533b0764cc28672d4a13e377338e5a10
Tomáš Wróbel <wrobel@programmer.net>
2023-05-05 09:20:44 +0200

    First version

---
+++ README.md
@@ -0,0 +1,97 @@
+# Parley.js
+
+## An oversimplified library for HTML5 dialogs
+
+Parley.js is a small utility replacing `window.prompt` and `window.confirm`.
+
+## Minimum requirements
+
+The `Parley` class cannot be placed inside a module. It must be globally available.
+
+|            | Needs      | Version | MDN                           |
+| ---------- | ---------- | ------- | ----------------------------- |
+| ECMAScript | `Promise`  | ES2015  | [see](https://mdn.io/Promise) |
+| HTML       | `<dialog>` | HTML5   | [see](https://mdn.io/dialog)  |
+
+If you do not use transpiler like Babel or TypeScript, note that code uses ES6 class.
+
+## API
+
+Everything starts with `<script>` tag:
+
+```html
+<script src="https://unpkg.com/parley.js"></script>
+```
+
+Note it must be a classic script, not a module.
+
+You should also include CSS:
+
+```html
+<link rel="stylesheet" href="https://unpkg.com/parley.js/dist/layout.css" />
+<!-- Next line is optional -->
+<link rel="stylesheet" href="https://unpkg.com/parley.js/dist/basic.css" />
+```
+
+### `Parley.fire`
+
+It takes configuration and responds with a Promise. Depending on the input type, configuration and
+output vary. But if the parley was cancelled (by the cancel button or the escape key) the output is
+always false.
+
+#### Base configuration
+
+| Name                | Description                                  | Default     |
+| ------------------- | -------------------------------------------- | ----------- |
+| `title`             | The title displayed in parley. Supports HTML | `""`        |
+| `message`           | The HTML message of the parley.              | `""`        |
+| `cancelButtonHTML`  | The HTML content of the cancel button.       | `Cancel"`   |
+| `confirmButtonHTML` | The HTML content of the confirm button.      | `"OK"`      |
+| `value`             | The default value.                           | `undefined` |
+| `inputOptions`      | The input-type-specific; see bellow          | {}          |
+
+#### Input options
+
+| Input types                                        | Configuration type | Output / Defaut value type        |
+| -------------------------------------------------- | ------------------ | --------------------------------- |
+| `"number"` `"range"`                               | `NumberOptions`    | `number`                          |
+| `"textarea"`                                       | `TextareaOptions`  | `string`                          |
+| `"tel"` `"text"` `"email"` `"search"` `"password"` | `TextOptions`      | `string`                          |
+| `"checkbox"` `"select"` `"radio"`                  | `SelectOptions`    | `string`, `string[]` for checkbox |
+| `"none`                                            | `never`            | `boolean`                         |
+
+```ts
+interface NumberOptions {
+    min?: number;
+    max?: number;
+    step?: number;
+    placeholder?: string;
+}
+
+interface TextareaOptions {
+    rows?: number;
+    cols?: number;
+    placeholder?: string;
+}
+
+interface TextOptions {
+    /**
+     * Regular expression used to validate the input
+     */
+    pattern?: RegExp;
+    placeholder?: string;
+}
+
+interface SelectionOptions {
+    /**
+     * The key is the value of the input.
+     * The value is the label html of the input.
+     */
+    [value: string]: string;
+}
+```
+
+### `Parley.close` and `Parley.cancel`
+
+Both functions close the parley immediately. While `Parley.cancel()` causes to return `false`,
+`Parley.close()` returns the user value.
---
+++ src/parley.ts
@@ -0,0 +1,312 @@
+/**
+ * @license MIT
+ * @version 1.0
+ * @description A simple dialog utility for the web.
+ * @example
+ * ```ts
+ * const result = await Parley.fire({
+ * 	title: "Enter a number",
+ * 	message: "Enter a number between 1 and 10",
+ * 	input: "number",
+ * 	inputOptions: {
+ * 		min: 1,
+ * 		max: 10
+ * 	}
+ * });
+ * ```
+ */
+/**
+ * Parley is a **static** class
+ * providing the {@link Parley.fire fire} method
+ */
+class Parley {
+    protected static element = document.createElement("dialog");
+    protected static title = document.createElement("h2");
+    protected static form = document.createElement("form");
+    protected static message = document.createElement("p");
+    protected static confirmButton = document.createElement("button");
+    protected static cancelButton = document.createElement("button");
+    /**
+     * Initializes the dialog DOM. Automatically called
+     * by {@link fire} if you have not initialized yet
+     * @param root The root element to append the dialog to. Defaults to body.
+     */
+    public static init(root?: Element | null) {
+        this.element.appendChild(this.title);
+        this.element.appendChild(this.message);
+        this.element.appendChild(this.form);
+
+        this.form.action = `javascript:${this.name}.close()`;
+        this.cancelButton.formMethod = "dialog";
+        this.cancelButton.type = "submit";
+        this.cancelButton.value = "false";
+        this.confirmButton.type = "submit";
+
+        (root || document.body).appendChild(this.element);
+    }
+    /**
+     * Creates a prompt dialog.
+     * @template I The input type.
+     * @param options The options of the dialog.
+     * @returns A promise that resolves to the input value or false if the dialog was cancelled.
+     */
+    public static fire<I extends keyof Parley.Inputs>(options: Parley.Options<I>) {
+        if (!this.element.parentElement) {
+            this.init();
+        } else {
+            this.form.innerHTML = "";
+        }
+
+        if (this.isInputType(options, "select")) {
+            const select = document.createElement("select");
+            select.name = "string";
+
+            for (const option in options.inputOptions) {
+                const optionElement = document.createElement("option");
+                optionElement.value = option;
+                optionElement.innerHTML = options.inputOptions[option];
+                select.appendChild(optionElement);
+            }
+
+            this.form.appendChild(select);
+        }
+
+        if (this.isInputType(options, "textarea")) {
+            const textarea = document.createElement("textarea");
+            textarea.name = "string";
+            if (options.inputOptions) {
+                if (options.inputOptions.placeholder) {
+                    textarea.placeholder = options.inputOptions.placeholder;
+                }
+                if (options.inputOptions.cols) {
+                    textarea.cols = options.inputOptions.cols;
+                }
+                if (options.inputOptions.rows) {
+                    textarea.rows = options.inputOptions.rows;
+                }
+            }
+            if (options.value) {
+                textarea.value = options.value;
+            }
+            this.form.appendChild(textarea);
+        }
+
+        if (this.isInputType(options, "checkbox", "radio")) {
+            for (const option in options.inputOptions) {
+                const label = document.createElement("label");
+                const input = document.createElement("input");
+                label.innerHTML = options.inputOptions[option];
+
+                input.type = options.input;
+                input.value = option;
+
+                if (options.input === "radio") {
+                    input.name = "string";
+                    input.checked = options.value === option;
+                } else {
+                    input.name = "array";
+                    input.checked = options.value?.indexOf(option) !== -1;
+                }
+
+                label.prepend(input);
+                this.form.appendChild(label);
+            }
+        }
+
+        if (this.isInputType(options, "number", "range")) {
+            const input = document.createElement("input");
+            input.type = options.input;
+            input.name = "number";
+            if (options.inputOptions) {
+                if (options.inputOptions.placeholder) {
+                    input.placeholder = options.inputOptions.placeholder;
+                }
+                if (options.inputOptions.min) {
+                    input.min = options.inputOptions.min.toString();
+                }
+                if (options.inputOptions.max) {
+                    input.max = options.inputOptions.max.toString();
+                }
+                if (options.inputOptions.step) {
+                    input.step = options.inputOptions.step.toString();
+                }
+            }
+            if (options.value) {
+                input.value = `${options.value}`;
+            }
+            this.form.appendChild(input);
+        }
+
+        if (this.isInputType(options, "tel", "email", "search", "text", "password")) {
+            const input = document.createElement("input");
+            if (options.inputOptions) {
+                if (options.inputOptions.placeholder) {
+                    input.placeholder = options.inputOptions.placeholder;
+                }
+                if (options.inputOptions.pattern) {
+                    input.pattern = options.inputOptions.pattern.source;
+                }
+            }
+            if (options.value) {
+                input.value = options.value;
+            }
+            input.type = options.input;
+            input.name = "string";
+            this.form.appendChild(input);
+        }
+
+        this.title.innerHTML = options.title || "";
+        this.message.innerHTML = options.message || "";
+        this.cancelButton.innerHTML = options.cancelButtonHTML || "";
+        this.confirmButton.innerHTML = options.confirmButtonHTML || "OK";
+
+        const buttonContainer = document.createElement("div");
+        buttonContainer.appendChild(this.confirmButton);
+        buttonContainer.appendChild(this.cancelButton);
+        buttonContainer.className = "button-container";
+        this.form.appendChild(buttonContainer);
+
+        return new Promise<Parley.Inputs[I][0] | false>(resolve => {
+            this.element.addEventListener(
+                "close",
+                function () {
+                    resolve(JSON.parse(this.returnValue || "false"));
+                    this.returnValue = "";
+                },
+                {once: true}
+            );
+            this.element.showModal();
+        });
+    }
+    /**
+     * Programatically closes the dialog.
+     */
+    public static close() {
+        const data = new FormData(this.form);
+
+        if (data.has("number")) {
+            this.element.close(`${data.get("number") || "0"}`);
+        } else if (data.has("string")) {
+            this.element.close(JSON.stringify(data.get("string")));
+        } else if (data.has("array")) {
+            this.element.close(JSON.stringify(data.getAll("array")));
+        } else if (this.form.querySelector("input")) {
+            this.element.close("[]");
+        } else {
+            this.element.close("true");
+        }
+    }
+	/**
+	 * Closes the dialog and returns with `false`.
+	 */
+	public static cancel() {
+		this.element.close();
+	}
+    protected static isInputType<I extends keyof Parley.Inputs>(
+        options: Parley.Options,
+        ...inputs: I[]
+    ): options is Parley.Options<I> & {input: I} {
+        return inputs.indexOf(options.input) !== -1;
+    }
+    /**
+     * Is dialog open?
+     */
+    public static isOpen() {
+        return this.element.open;
+    }
+    /**
+     * Never construct Parley class!
+     * @throws {TypeError}
+     */
+    private constructor() {
+        throw new TypeError("Illegal constructor");
+    }
+}
+
+declare namespace Parley {
+    /**
+     * Map of input types to their options.
+     */
+    interface Inputs {
+        none: [boolean, never];
+        range: [number, NumberOptions];
+        number: [number, NumberOptions];
+        tel: [string, TextOptions];
+        text: [string, TextOptions];
+        email: [string, TextOptions];
+        search: [string, TextOptions];
+        password: [string, TextOptions];
+        radio: [string, SelectionOptions];
+        select: [string, SelectionOptions];
+        textarea: [string, TextareaOptions];
+        checkbox: [string[], SelectionOptions];
+    }
+
+    interface Options<I extends keyof Inputs = any> {
+        /**
+         * The HTML title of the dialog.
+         * @default ""
+         */
+        title?: string;
+        /**
+         * The HTML message of the dialog.
+         * @default ""
+         */
+        message?: string;
+        /**
+         * The HTML content of the dialog.
+         * @default "Cancel"
+         */
+        cancelButtonHTML?: string;
+        /**
+         * The HTML content of the dialog.
+         * @default "OK"
+         */
+        confirmButtonHTML?: string;
+        /**
+         * The input type of the dialog.
+         */
+        input?: I;
+        /**
+         * The default value of the input.
+         */
+        value?: Inputs[I][0];
+        /**
+         * The options of the input.
+         * It is required for these types:
+         * * select
+         * * checkbox
+         * * radio
+         */
+        inputOptions?: Inputs[I][1];
+    }
+
+    interface NumberOptions {
+        min?: number;
+        max?: number;
+        step?: number;
+        placeholder?: string;
+    }
+
+    interface TextareaOptions {
+        rows?: number;
+        cols?: number;
+        placeholder?: string;
+    }
+
+    interface TextOptions {
+        /**
+         * Regular expression used to validate the input
+         */
+        pattern?: RegExp;
+        placeholder?: string;
+    }
+
+    interface SelectionOptions {
+        /**
+         * The key is the value of the input.
+         * The value is the label html of the input.
+         */
+        [value: string]: string;
+    }
+}
